29cc1d03698d902bc9261fcf87bb0aa1d2333530
wongee93 <jhwon9304@gmail.com>
2023-03-09 15:44:14 +0900

    setting tailwind.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
+      <header className="text-gray-100 bg-blue-500 text-center p-5 align-middle">
+        tailwind test
+      </header>
       <h1>test</h1>
     </div>
   );
