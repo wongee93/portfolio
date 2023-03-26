caa9d6bba23cbbd57826a2e932cb2b11c4b8cd3c
wongee93 <jhwon9304@gmail.com>
2023-03-26 11:52:57 +0900

    Add Skill Page

--- src/Skill.js
+++ src/Skill.js
@@ -1,7 +1,48 @@
 export default function Skill() {
     return (
-        <>
-            <h1>Skill</h1>
-        </>
+        <div class="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3 sm:px-0">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-10">
+                <img class="card-image" src="https://daedaem.netlify.app/static/54a24310f75e8c5b77a88f60776ae4c3/ab163/Typescript_logo.png" alt="Typescript" />
+                <div class="px-6 py-4 text-center">
+                    <div class="font-bold text-xl mb-2">Typescript</div>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-10">
+                <img class="card-image" src="https://blog.kakaocdn.net/dn/dpwvVE/btrBqolp4WG/xU2kPsR8hJ0Rpx9B1LSoZ1/img.png" alt="React" />
+                <div class="px-6 py-4 text-center">
+                    <div class="font-bold text-xl mb-2">React</div>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-10">
+                <img class="card-image" src="https://images.velog.io/images/jay/post/3a497590-d1b6-414c-9f3f-7b6c7eb18f6d/img.png" alt="NextJS" />
+                <div class="px-6 py-4 text-center">
+                    <div class="font-bold text-xl mb-2">NextJS</div>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-10">
+                <img class="card-image" src="https://images.velog.io/images/offdutybyblo/post/65c734fd-077c-4a68-8b3b-557c52428511/htmljscss.jpeg" alt="HTML/CSS/JS" />
+                <div class="px-6 py-4 text-center">
+                    <div class="font-bold text-xl mb-2">HTML/CSS/JS</div>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-10">
+                <img class="card-image" src="https://www.zigae.com/static/7666417ed492feef76bbe9fd146a4192/e9beb/styledcomponents.png" alt="Styled Components" />
+                <div class="px-6 py-4 text-center">
+                    <div class="font-bold text-xl mb-2">Styled Components</div>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-10">
+                <img class="card-image" src="https://velog.velcdn.com/images/real-bird/post/c4f14195-c5cc-4507-a3e0-5eda208f99ef/image.jpg" alt="Tailwind" />
+                <div class="px-6 py-4 text-center">
+                    <div class="font-bold text-xl mb-2">Tailwind CSS</div>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-10">
+                <img class="card-image" src="https://blog.kakaocdn.net/dn/wNwMg/btqEq78ORcI/qZeWrKT8Oj97E9fZIdaZY0/img.png" alt="jQuery" />
+                <div class="px-6 py-4 text-center">
+                    <div class="font-bold text-xl mb-2">jQuery</div>
+                </div>
+            </div>
+        </div>
     )
 }
