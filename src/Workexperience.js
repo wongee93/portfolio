a0d580650273fdd89bac530ce498c31202f6b96f
wongee93 <jhwon9304@gmail.com>
2023-04-03 14:06:45 +0900

    Add Education page

--- src/Workexperience.js
+++ src/Workexperience.js
@@ -1,38 +1,36 @@
 export default function Workexperience() {
     return (
-        <>
-            <div class="divide-y divide-gray-100">
-                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt class="mt-6 ml-10 text-3xl font-semibold leading-6 text-gray-900 sm:mt-0 sm:col-span-1">KoreaPDS</dt>
-                </div>
+        <div class="divide-y divide-gray-100">
+            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt class="mt-6 ml-10 text-3xl font-semibold leading-6 text-gray-900 sm:mt-0 sm:col-span-1">KoreaPDS</dt>
+            </div>
 
-                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <div class="mt-1 ml-10 text-lg text-black-700 sm:col-span-2 sm:mt-0">
-                        <p><span class="text-gray-500 font-semibold">2019.08-2022.06</span></p>
-                        <p class="mt-8 text-xl">
-                            <span class="text-red-500 font-semibold">서비스 기획</span> 및 <span class="text-red-500 font-semibold">솔루션 업무</span> 담당
-                        </p>
-                        <p class="mt-3 text-gray-500">
-                            - 건설사(GS건설, SK에코플랜트 등), F&B(오뚜기, LG생활건강 등) 외 다수 업체에 원자재 <span class="text-red-500">구매 컨설팅</span> 제공
-                        </p>
-                        <p class="mt-8 text-xl">
-                            서비스 <span class="text-red-500 font-semibold">홍보/마케팅</span> 업무 담당
-                        </p>
-                        <p class="mt-3 text-gray-500">
-                            - 원자재 가격 전망 <span class="text-red-500">AI 서비스 홍보</span>를 위한 코엑스 인공지능 박람회 참가
-                        </p>
-                        <p class="mt-3 text-gray-500">
-                            - 매년 원자재 가격 전망 세미나/교육 진행
-                        </p>
-                        <p class="mt-8 text-xl">
-                            서비스 개발을 위한 IT부서 및 다양한 부서와 <span class="text-red-500 font-semibold">협업 경험</span>
-                        </p>
-                        <p class="mt-3 text-gray-500">
-                            - AI, 패턴분석을 이용한 원자재 가격전망 서비스 개발
-                        </p>
-                    </div>
+            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <div class="mt-1 ml-10 text-xl text-black-700 sm:col-span-2 sm:mt-0">
+                    <p><span class="text-gray-500 font-semibold">2019.08-2022.06</span></p>
+                    <p class="mt-8 text-xl">
+                        <span class="text-red-500 font-semibold">서비스 기획</span> 및 <span class="text-red-500 font-semibold">솔루션 업무</span> 담당
+                    </p>
+                    <p class="mt-3 text-gray-500">
+                        - 건설사(GS건설, SK에코플랜트 등), F&B(오뚜기, LG생활건강 등) 외 다수 업체에 원자재 <span class="text-red-500">구매 컨설팅</span> 제공
+                    </p>
+                    <p class="mt-8 text-xl">
+                        서비스 <span class="text-red-500 font-semibold">홍보/마케팅</span> 업무 담당
+                    </p>
+                    <p class="mt-3 text-gray-500">
+                        - 원자재 가격 전망 <span class="text-red-500">AI 서비스 홍보</span>를 위한 코엑스 인공지능 박람회 참가
+                    </p>
+                    <p class="mt-3 text-gray-500">
+                        - 매년 원자재 가격 전망 세미나/교육 진행
+                    </p>
+                    <p class="mt-8 text-xl">
+                        서비스 개발을 위한 IT부서 및 다양한 부서와 <span class="text-red-500 font-semibold">협업 경험</span>
+                    </p>
+                    <p class="mt-3 text-gray-500">
+                        - AI, 패턴분석을 이용한 원자재 가격전망 서비스 개발
+                    </p>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
